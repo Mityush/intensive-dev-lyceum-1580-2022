6db47594df23a4d7cb66c737c252536ac9621d7e
Mityush <120013339+Mityush@users.noreply.github.com>
2022-12-14 14:24:24 +0300

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,16 +1,17 @@
-def tree(n:int):
-    if isinstance(n, int) or isinstance(n, str) and n.isdigit():
-        if n < 3:
-            return 'Null'
-        else:
-            n = int(n)
-            k_st = 1
-            k_sp = n*2 - 2 
-            s=''
-            for i in range(1,n):
-                s += k_sp // 2 * ' ' + k_st * '*' + k_sp // 2 * ' ' + '\n'
-                k_st+=2
-                k_sp-=2
-            s+= (n - 1) * ' ' + '|' + (n-1) * ' '
-            return s
-    else: return 'TYPE_ERROR'
+def tree(n):
+    if not isinstance(n, int) and not isinstance(n, str) or not n.isdigit():
+        return 'TYPE_ERROR'
+    n = int(n)
+    if n < 3:
+        return none
+    else:
+        n = int(n)
+        k_st = 1
+        k_sp = n*2 - 2
+        s=''
+        for i in range(1,n):
+            s += k_sp // 2 * ' ' + k_st * '*' + k_sp // 2 * ' ' + '\n'
+            k_st += 2
+            k_sp -= 2
+        s+= (n - 1) * ' ' + '|' + (n-1) * ' '
+        return s
