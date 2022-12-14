62395534dd3226af014b99368b2db7b9cba287af
Mityusha <120013339+Mityush@users.noreply.github.com>
2022-12-14 15:22:29 +0300

    Create range.py

---
+++ range.py
@@ -0,0 +1,12 @@
+
+def range(end, start = 0, step = 1, isRight = False):
+    if isRight:
+        for i in range()
+    else:
+        for i in range(start, end, step):
+            a[i] = i
+def main():
+    range(4)
+
+if __name__ == "__main__":
+    main()
